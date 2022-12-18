407ed55dfb436e2156fea5b06a335a54a34ec364
Alexandra <aspudka@gmail.com>
2022-12-18 16:39:40 +0200

    initial commit

---
+++ 18-12/js/index.js
@@ -0,0 +1,14 @@
+//import {sum} from './math.js'
+
+//console.log(sum(1,2));
+//console.log(mult(PI, 2));
+
+//import * as MyMath from './math.js';
+
+//console.log(MyMath.sum(MyMath.PI, 3));
+
+import MyTest, { sum, mult, PI } from "./math.js";
+
+console.log(mult(PI, 2));
+console.log(new MyTest(5));
+// console.log("first", first);
---
+++ 18-12/js/math.js
@@ -0,0 +1,16 @@
+const sum = (a, b)=> a + b;
+
+
+const mult = (a, b) => a * b;
+
+const PI = 3.14;
+
+export { sum, mult, PI };
+
+class Test {
+  constructor(tmp){
+    this.tmp = tmp;
+  }
+}
+
+export default Test;
